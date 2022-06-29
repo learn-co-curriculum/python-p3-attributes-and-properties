dc8c8b82317f218f6429dda136a1a2182e7a137e
Ben Botsford <ben.botsford@flatironschool.com>
2022-06-29 13:28:38 -0500

    clear solutions

--- lib/dog.py
+++ lib/dog.py
@@ -1,39 +1,4 @@
 #!/usr/bin/env python
 
 class Dog:
-
-    breed_list = [
-        "Mastiff",
-        "Chihuahua",
-        "Corgi",
-        "Shar Pei",
-        "Beagle",
-        "French Bulldog",
-        "Pug",
-        "Pointer",
-    ]
-
-    def __init__(self):
-        self._name = "Unnamed"
-        self._breed = "Mutt"
-
-    def get_name(self):
-        return self._name
-
-    def set_name(self, name):
-        if (type(name) == str) and (1 <= len(name) <= 25):
-            self._name = name
-        else:
-            print("Name must be string between 1 and 25 characters.")
-
-    def get_breed(self):
-        return self._breed
-
-    def set_breed(self, breed):
-        if breed in Dog.breed_list:
-            self._breed = breed
-        else:
-            print("Breed must be in list of approved breeds.")
-
-    name = property(get_name, set_name,)
-    breed = property(get_breed, set_breed)
+    pass
