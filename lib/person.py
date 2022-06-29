f1042478162eb2090985f796504e054bff4133f1
Ben Botsford <ben.botsford@flatironschool.com>
2022-06-29 13:18:11 -0500

    finish tests

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python
 
 class Person:
-    pass
+
+    job_list = [
+        "Admin",
+        "Finance",
+        "Customer Service",
+        "Sales",
+        "Human Resources",
+        "General Management",
+        "ITC",
+        "Research & Development",
+        "Production",
+        "Marketing",
+        "Legal",
+        "Purchasing",
+    ]
+
+    def __init__(self):
+        self._name = "Nobody"
+        self._job = "Unemployed"
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if (type(name) == str) and (1 <= len(name) <= 25):
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in Person.job_list:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(get_name, set_name,)
+    job = property(get_job, set_job)
--- testing/person_test.py
+++ testing/person_test.py
@@ -2,18 +2,67 @@
 
 from lib.person import Person
 
-# class TestPerson:
-#     '''Person in person.py'''
+import io
+import sys
 
-#     def test_is_class(self):
-#         '''is a class with the name "Person"'''
-#         guido = Person("Guido")
-#         assert(type(guido) == Person)
+class TestPerson:
+    '''Person in person.py'''
 
-# class TestInit:
-#     '''Person.__init__ in person.py'''
+    def test_is_class(self):
+        '''is a class with the name "Person".'''
+        guido = Person()
+        assert(type(guido) == Person)
+        
+    def test_name_not_empty(self):
+        '''prints "Name must be string between 1 and 25 characters." if empty string.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        guido = Person()
+        guido.name = ""
+        sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
 
-#     def test_saves_self_name(self):
-#         '''takes a name as an argument and saves it to self.name'''
-#         guido = Person("Guido")
-#         assert(guido.name == "Guido")
+    def test_name_string(self):
+        '''prints "Name must be string between 1 and 25 characters." if not string.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        guido = Person()
+        guido.name = 123
+        sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
+
+    def test_name_under_25(self):
+        '''prints "Name must be string between 1 and 25 characters." if string over 25 characters.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        guido = Person()
+        guido.name = "What do Persons do on their day off? Can't lie around - that's their job."
+        sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
+
+    def test_valid_name(self):
+        '''saves name if string between 1 and 25 characters.'''
+        guido = Person()
+        guido.name = "Guido"
+        assert(guido.name == "Guido")
+
+    def test_valid_name_title_case(self):
+        '''converts name to title case and saves if between 1 and 25 characters'''
+        guido = Person()
+        guido.name = "guido van rossum"
+        assert(guido.name == "Guido Van Rossum")
+
+    def test_job_not_in_list(self):
+        '''prints "Job must be in list of approved jobs." if not in job list.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        guido = Person()
+        guido.job = "Benevolent dictator for life"
+        sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "Job must be in list of approved jobs.\n")
+
+    def test_job_in_list(self):
+        '''saves job if in job list.'''
+        guido = Person()
+        guido.job = "ITC"
+        assert(guido.job == "ITC")
